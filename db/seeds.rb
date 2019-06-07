aaa47bce5a6c55372405a3f859c75ff3570635d0
giapbnv <nguyen.van.giapb@sun-asterisk.com>
2019-06-07 10:57:23 +0700

    dev 2: create, view posts with comment and paging ver 3 UPDATE Fix Paginate with Ajax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,28 @@
 User.create(name: "giapb",email: "giapb@mail.com", password:"Aa123456")
 
 90.times do |x|
-  User.create(name: "username #{x}", email: "email#{x}@mail.com", password:"Bb123456")
+  User.create(name: Faker::Name.name, email: "email#{x}@mail.com", password:"Bb123456")
 end
 
 users = User.all
 users.each do |u|
-  20.times do |x|
-    e = u.entries.build title: "The tittle #{x}", content: "This is content of #{x}"
+  11.times do |x|
+    e = u.entries.build title: Faker::Lorem.sentence, content: Faker::Lorem.sentence
     e.save
   end
 end
 
 entries = Entry.all
 entries.each do |e|
-  50.times do |i|
-    r = rand 50
-    c = e.comments.build user_id: r,content: "this is comment #{i}"
+  21.times do |i|
+    r = rand 90
+    c = e.comments.build user_id: r,content: Faker::Lorem.question
     c.save
   end
 end
 
-1000.times do |x|
-  r1 = rand 89
-  r2 = rand 89
+10.times do |x|
   u1 = User.find_by id: r1
   u2 = User.find_by id: r2
-  u1.follow u2
+  u1.follow u2 unless (r1 == r2 or r1 * r2 == 0)
 end
