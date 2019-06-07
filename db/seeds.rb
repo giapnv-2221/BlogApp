a95e9cc1014ec5fed2deab108c5c81fc2933f9be
giapbnv <nguyen.van.giapb@sun-asterisk.com>
2019-06-07 10:13:13 +0700

    dev 2: create, view posts with comment and paging ver 2 UPDATE

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 User.create(name: "giapb",email: "giapb@mail.com", password:"Aa123456")
 
-9.times do |x|
+90.times do |x|
   User.create(name: "username #{x}", email: "email#{x}@mail.com", password:"Bb123456")
 end
 
 users = User.all
 users.each do |u|
-  9.times do |x|
+  20.times do |x|
     e = u.entries.build title: "The tittle #{x}", content: "This is content of #{x}"
     e.save
   end
@@ -14,9 +14,17 @@ end
 
 entries = Entry.all
 entries.each do |e|
-  5.times do |i|
-    r = rand 5
+  50.times do |i|
+    r = rand 50
     c = e.comments.build user_id: r,content: "this is comment #{i}"
     c.save
   end
+end
+
+1000.times do |x|
+  r1 = rand 89
+  r2 = rand 89
+  u1 = User.find_by id: r1
+  u2 = User.find_by id: r2
+  u1.follow u2
 end
